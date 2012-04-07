c9371af2bc1b5e49d5d9f8308f050c9c9f6cec1f
gbuntu127 <gbuntu127@gmail.com>
2012-04-06 20:31:57 -0500

    Update gvim.setup.win.cmd

--- gvim.setup.win.cmd
+++ gvim.setup.win.cmd
@@ -2,7 +2,7 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set BASE_DIR=%HOME%\gvim
-call git clone git://github.com/ %BASE_DIR%
+call git clone git://github.com/gbuntu127/gvim.git %BASE_DIR%
 call mkdir %BASE_DIR%\vim\bundle
 call mklink /J %HOME%\vim %BASE_DIR%\vim
 call mklink %HOME%\.vimrc %BASE_DIR%\vimrc
